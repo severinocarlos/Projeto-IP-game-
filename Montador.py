76f8afe6706331628bfe329b9cf29edb66130be1
Severino Carlos <63283908+severinocarlos@users.noreply.github.com>
2021-08-26 22:17:40 -0300

    finish game

--- Montador.py
+++ Montador.py
@@ -1,12 +1,12 @@
 import pygame as pg
 import time
-from NovoSistemaArquitetado.Componentes.Janela.Logica import LogicaTela as LT
-from NovoSistemaArquitetado.Componentes.Jogador.Logica import AtualizarJogador as AJ
-from NovoSistemaArquitetado.Componentes.Inimigo.Logica import AtualizarInimigo as AI
-from NovoSistemaArquitetado.Componentes.HUD.Logica import LogicaContador as LC
-from NovoSistemaArquitetado.Componentes.Colecionaveis.Logica import AtualizarPokebola as AP1
-from NovoSistemaArquitetado.Componentes.Colecionaveis.Logica import AtualizarPokemon as AP2
-from NovoSistemaArquitetado.Componentes.Pokebola.Logica import AtualizarPokebolaArremessavel as APb
+from NovoSistemaArquitetado.Janela.Logica import LogicaTela as LT
+from NovoSistemaArquitetado.Jogador.Logica import AtualizarJogador as AJ
+from NovoSistemaArquitetado.Inimigo.Logica import AtualizarInimigo as AI
+from NovoSistemaArquitetado.HUD.Logica import LogicaContador as LC
+from NovoSistemaArquitetado.Colecionaveis.Logica import AtualizarPokebola as AP1
+from NovoSistemaArquitetado.Colecionaveis.Logica import AtualizarPokemon as AP2
+from NovoSistemaArquitetado.Pokebola.Logica import AtualizarPokebolaArremessavel as APb
 
 def AtualizarJanela():
     LT.CarregarMapa()
@@ -20,7 +20,7 @@ def AtualizarJanela():
 
 def Montar():
     TempoInicio = time.time()
-    from NovoSistemaArquitetado.Componentes.Sons.Logica import LogicaMusica
+    from NovoSistemaArquitetado.Sons.Logica import LogicaMusica
     AJ.AtualizarJogador()
     AI.AtualizarInimigo()
     AP1.AtualizarPokebola()
